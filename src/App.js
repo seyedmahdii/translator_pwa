529534f1d9ef98a9c00b5f3d0b973c81b054dbb8
Seyed Mahdi Jalali <seyedmahdijalali2020@gmail.com>
2021-07-16 17:37:16 +0430

    UI added.

--- src/App.js
+++ src/App.js
@@ -1,20 +1,139 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./App.css";
-import { translate } from "./api/index";
+import { getLanguages, translate } from "./api/index";
+
+import { IconButton } from "@material-ui/core";
+import SwapHorizRoundedIcon from "@material-ui/icons/SwapHorizRounded";
+import FileCopyRoundedIcon from "@material-ui/icons/FileCopyRounded";
+import DeleteOutlineRoundedIcon from "@material-ui/icons/DeleteOutlineRounded";
+import VolumeUpRoundedIcon from "@material-ui/icons/VolumeUpRounded";
 
 function App() {
-    const search = async (event) => {
-        const {
-            data: {
-                translations: { translatedText },
-            },
-        } = await translate("en", "fa", "hello baby");
-        console.log(translatedText);
+    const [languages, setLanguages] = useState([
+        { language: "en", name: "english" },
+        { language: "fa", name: "farsi" },
+        { language: "es", name: "espanish" },
+    ]);
+    const [sourceLan, setSourceLan] = useState("en");
+    const [targetLan, setTargetLan] = useState("fa");
+    const [text, setText] = useState("");
+    const [translatedText, setTranslatedText] = useState("");
+
+    const fetchTranslatedText = async () => {
+        const data = await translate(sourceLan, targetLan, "hello baby");
+        console.log(data);
+        // console.log(translatedText);
     };
 
+    const fetchLanguages = async () => {
+        const languages = await getLanguages();
+        setLanguages(languages);
+        console.log(languages);
+    };
+
+    useEffect(() => {
+        // fetchLanguages();
+    }, []);
+
     return (
-        <div>
-            <h1>translate</h1>
+        <div className="container">
+            <main className="content">
+                <header className="header">
+                    <div className="source-lan__wrapper">
+                        <select
+                            onChange={(e) => setSourceLan(e.target.value)}
+                            className="lan__select source-lan"
+                        >
+                            {languages?.map((language, index) => {
+                                return (
+                                    <option
+                                        value={language.language}
+                                        key={index}
+                                        className="lan__option"
+                                    >
+                                        {language.name}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    </div>
+
+                    <div className="header__swap">
+                        <SwapHorizRoundedIcon className="swap-icon" />
+                    </div>
+
+                    <div className="target-lan__wrapper">
+                        <select
+                            onChange={(e) => setTargetLan(e.target.value)}
+                            className="lan__select target-lan"
+                        >
+                            {languages.map((language, index) => {
+                                return (
+                                    <option
+                                        value={language.language}
+                                        key={index}
+                                        className="lan__option"
+                                    >
+                                        {language.name}
+                                    </option>
+                                );
+                            })}
+                        </select>
+                    </div>
+                </header>
+
+                <div className="body">
+                    <div className="box">
+                        <textarea
+                            name="source"
+                            className="box__input"
+                            placeholder={`Enter text (${sourceLan})`}
+                            value={text}
+                            onChange={(e) => setText(e.target.value)}
+                        ></textarea>
+
+                        <div className="box__footer">
+                            <IconButton onClick={() => setText("")}>
+                                <DeleteOutlineRoundedIcon className="footer-icon" />
+                            </IconButton>
+                            <IconButton
+                                onClick={() =>
+                                    navigator.clipboard.writeText(text)
+                                }
+                            >
+                                <FileCopyRoundedIcon className="footer-icon" />
+                            </IconButton>
+                        </div>
+                    </div>
+
+                    {translatedText && (
+                        <div className="box">
+                            <h2 className="box__result">{translatedText}</h2>
+
+                            <div className="box__footer">
+                                <IconButton
+                                    onClick={() =>
+                                        alert(
+                                            "This option will be here soon :)"
+                                        )
+                                    }
+                                >
+                                    <VolumeUpRoundedIcon className="footer-icon" />
+                                </IconButton>
+                                <IconButton
+                                    onClick={() =>
+                                        navigator.clipboard.writeText(
+                                            "translated Text"
+                                        )
+                                    }
+                                >
+                                    <FileCopyRoundedIcon className="footer-icon" />
+                                </IconButton>
+                            </div>
+                        </div>
+                    )}
+                </div>
+            </main>
         </div>
     );
 }
--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,15 @@
 import axios from "axios";
+import dotenv from "dotenv";
 
 export const translate = async (from, to, text) => {
+    dotenv.config();
+
     const options = {
         method: "POST",
         url: "https://deep-translate1.p.rapidapi.com/language/translate/v2",
         headers: {
             "content-type": "application/json",
-            "x-rapidapi-key":
-                "582385dcdamshe93967ba0316e1ap116a42jsnf4290f1aeafb",
+            "x-rapidapi-key": process.env.RAPIDAPI_KEY,
             "x-rapidapi-host": "deep-translate1.p.rapidapi.com",
         },
         data: { q: text, source: from, target: to },
@@ -15,7 +17,28 @@ export const translate = async (from, to, text) => {
 
     try {
         const { data } = await axios.request(options);
+        console.log(data);
         return data;
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const getLanguages = async () => {
+    const options = {
+        method: "GET",
+        url: "https://deep-translate1.p.rapidapi.com/language/translate/v2/languages",
+        headers: {
+            "x-rapidapi-key": process.env.RAPIDAPI_KEY,
+            "x-rapidapi-host": "deep-translate1.p.rapidapi.com",
+        },
+    };
+
+    try {
+        const {
+            data: { languages },
+        } = await axios.request(options);
+        return languages;
     } catch (error) {
         console.log(error);
     }
